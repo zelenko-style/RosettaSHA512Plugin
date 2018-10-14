26df0ee3ed2087a75a710c0584674e16a1c5afe3
zelenko-style <44140749+zelenko-style@users.noreply.github.com>
2018-10-14 19:20:29 +0300

    I made a few changes to the code. The conversion to hex will now result in a lowercase string.

--- CustomFixitySHA512Plugin.java
+++ CustomFixitySHA512Plugin.java
@@ -1,66 +1,133 @@
 package com.exlibris.dps.repository.plugin.fixity;
 
-import com.exlibris.core.sdk.strings.StringUtils;
-import com.exlibris.dps.repository.plugin.CustomFixityPlugin;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import javax.xml.bind.DatatypeConverter;
+import com.exlibris.dps.repository.plugin.CustomFixityPlugin;
+import com.exlibris.core.infra.common.exceptions.logging.ExLogger;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
+/**
+ * A custom implementation of the CustomFixityPlugin. This implementation will
+ * return the SHA512 checksum of the file.
+ *
+ * @author AlexZ
+ */
+public class CustomFixitySHA512Plugin implements CustomFixityPlugin {
 
-public class CustomFixitySHA512Plugin
-  implements CustomFixityPlugin
-{
-  private static final String PLUGIN_VERSION_INIT_PARAM = "PLUGIN_VERSION_INIT_PARAM";
-  private String pluginVersion = null;
-  private boolean result = true;
-  
-  private List<String> errors = null;
-  
+	private static final String PLUGIN_VERSION_INIT_PARAM = "PLUGIN_VERSION_INIT_PARAM";
+	private String pluginVersion = null;
+	private boolean result = true;
+	static ExLogger log = ExLogger.getExLogger(CustomFixitySHA512Plugin.class);
+	private List<String> errors = null;
 
-  public CustomFixitySHA512Plugin() {}
-  
-  public List<String> getErrors()
-  {
-    return errors;
-  }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.exlibris.dps.repository.plugin.CustomFixityPlugin#getErrors()
+	 */
+	@Override
+	public List<String> getErrors() {
+		return errors;
+	}
 
-  public String getFixity(String filePath, String oldFixity)
-    throws Exception
-  {
-    File file = new File(filePath);
-    
-    String newFixity = toHex(CustomFixitySHA512Plugin.HashFunction.access$2(CustomFixitySHA512Plugin.HashFunction.SHA512, file));
-    
-    if ((oldFixity != null) && (!newFixity.equals(oldFixity))) {
-      errors = new ArrayList();
-      errors.add("Fixity mismatch. Old fixity was " + oldFixity + ", new fixity is " + newFixity);
-    }
-    
-    return newFixity;
-  }
-  
-  public String getAlgorithm()
-  {
-    return "SHA-512";
-  }
-  
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.exlibris.dps.repository.plugin.CustomFixityPlugin#getFixity(java.lang.
+	 * String)
+	 */
+	@Override
+	public String getFixity(String filePath, String oldFixity) throws Exception {
 
-  public String getAgent()
-  {
-    return "Custom fixity SHA-512, Plugin Version " + pluginVersion;
-  }
-  
-  public void initParams(Map<String, String> initParams) {
-    pluginVersion = ((String)initParams.get("PLUGIN_VERSION_INIT_PARAM"));
-    if (!StringUtils.isEmptyString((String)initParams.get("fixityScanResult"))) {
-      result = Boolean.parseBoolean(((String)initParams.get("fixityScanResult")).trim());
-    }
-  }
+		File file = new File(filePath);
 
-  private static String toHex(byte[] bytes)
-  {
-    return DatatypeConverter.printHexBinary(bytes);
-  }
+		String newFixity = toHex(checksum(file));
+
+		if (!result) { // fail artificially if UI param "fixityScanResult" is set to FALSE
+			log.info("SHA-512: \"fixityScanResult\" is set to FALSE");
+			log.info("SHA-512 calculated for" + filePath + ". SHA-512 value is really:   " + newFixity);
+			newFixity = "DUMMY";
+			log.info("SHA-512 calculated for" + filePath + ". SHA-512 value will be now: " + newFixity);
+		}
+
+		if ((oldFixity != null) && (!newFixity.equalsIgnoreCase(oldFixity))) {
+			errors = new ArrayList();
+			errors.add("Fixity mismatch. Old fixity was " + oldFixity + ", new fixity is " + newFixity);
+		} else
+			log.info("SHA-512 calculated for " + filePath + ". SHA-512 value is: " + newFixity);
+
+		return newFixity;
+
+	}
+
+	@Override
+	public String getAlgorithm() {
+		return "SHA-512";
+	}
+
+	@Override
+	public String getAgent() {
+		return "Custom fixity SHA-512, Plugin Version " + pluginVersion;
+	}
+
+	public void initParams(Map<String, String> initParams) {
+		this.pluginVersion = initParams.get(PLUGIN_VERSION_INIT_PARAM);
+		 if(!StringUtils.isEmptyString(initParams.get("fixityScanResult"))){
+		 result = Boolean.parseBoolean(initParams.get("fixityScanResult").trim());
+		 }
+	}
+
+	private byte[] checksum(File input) throws IOException {
+		FileInputStream in = null;
+		try {
+			in = new FileInputStream(input);
+			MessageDigest digester = MessageDigest.getInstance("SHA-512");
+			byte[] block = new byte[4096];
+			int length = 0;
+
+			do {
+				length = in.read(block);
+				if (length > 0) {
+					if (digester != null) {
+						digester.update(block, 0, length);
+					}
+				}
+			} while (length > 0);
+			if (in != null)
+				in.close();
+
+			return digester.digest();
+		} catch (NoSuchAlgorithmException ex) {
+			log.error("SHA-512 not supported");
+		} catch (Exception e) {
+			log.error("SHA-512 could not be calculated");
+			e.printStackTrace();
+		}
+
+		finally {
+			if (in != null)
+				in.close();
+		}
+
+		return null;
+	}
+
+	private final static char[] hexArray = "0123456789abcdef".toCharArray();
+
+	private static String toHex(byte[] bytes) {
+		char[] hexChars = new char[bytes.length * 2];
+		for (int j = 0; j < bytes.length; j++) {
+			int v = bytes[j] & 0xFF;
+			hexChars[j * 2] = hexArray[v >>> 4];
+			hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+		}
+		return new String(hexChars);
+	}
+
 }
